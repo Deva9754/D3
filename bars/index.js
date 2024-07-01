9f74f00360f4286340ff40616ceb7324443d7b9a
Deva9754 <deva.sahu2001@gmail.com>
2024-07-01 14:59:13 +0530

    bars completed

--- bars/index.js
+++ bars/index.js
@@ -58,3 +58,69 @@ d3.json("data.json")
   .catch(function (error) {
     console.error("Error loading the data: " + error);
   });
+
+// ---------------- ARC -----------------
+
+// var canvas1 = d3
+//   .select("body")
+//   .append("svg")
+//   .attr("width", 500)
+//   .attr("height", 500);
+// var group = canvas1.append("g").attr("transform", "translate(100,100)");
+// var r = 100;
+// var p = Math.PI * 2;
+
+// var arc = d3
+//   .arc()
+//   .innerRadius(r - 20)
+//   .outerRadius(r)
+//   .startAngle(0)
+//   .endAngle(p - 1);
+
+// group.append("path").attr("d", arc);
+
+// ---------------------DONUT--------------------
+
+var data = [33, 34, 35];
+var r = 300;
+
+var color = d3.scaleOrdinal().range(["red", "blue", "green"]);
+
+var canvas2 = d3
+  .select("body")
+  .append("svg")
+  .attr("width", 1500)
+  .attr("height", 1000);
+
+var group = canvas2.append("g").attr("transform", "translate(300,300)");
+
+var arc = d3.arc().innerRadius(200).outerRadius(r);
+
+var pie = d3.pie().value(function (d) {
+  return d;
+});
+
+var arcs = group
+  .selectAll(".arc")
+  .data(pie(data))
+  .enter()
+  .append("g")
+  .attr("class", "arc");
+
+arcs
+  .append("path")
+  .attr("d", arc)
+  .attr("fill", function (d) {
+    return color(d.data);
+  });
+arcs
+  .append("text")
+  .attr("transform", function (d) {
+    return "translate(" + arc.centroid(d) + ")";
+  })
+  .attr("text-anchor", "middle")
+  .attr("font-size", "1.5rem")
+  .attr("fill", "white")
+  .text(function (d) {
+    return d.data;
+  });
