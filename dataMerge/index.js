ea49a52e796b1847919aee94b0fa74b0a5ca93a7
Deva9754 <deva.sahu2001@gmail.com>
2024-08-07 00:41:23 +0530

    datamerger with dataset

---
+++ dataMerge/index.js
@@ -0,0 +1,59 @@
+dataSet = [
+  { name: "Shanghai", population: 18, rank: 1 },
+  { name: "Devashish", population: 11, rank: 10 },
+  { name: "Nititn", population: 8, rank: 20 },
+  { name: "Prathmesh", population: 7, rank: 30 },
+  { name: "Nishchnt", population: 5, rank: 40 },
+  { name: "Peter", population: 4, rank: 50 },
+];
+
+var svgContainer = d3
+  .select("body")
+  .append("svg")
+  .attr("width", "250")
+  .attr("height", "200");
+
+var circles = svgContainer
+  .selectAll("circle")
+  .data(dataSet)
+  .enter()
+  .append("circle");
+
+circles
+  .attr("cx", function (d, i) {
+    return (i + 1) * 25;
+  })
+  .attr("cy", function (d, i) {
+    return (i + 1) * 25;
+  })
+  .attr("r", function (d, i) {
+    return d.population;
+  });
+
+var circleLabels = svgContainer
+  .selectAll("text")
+  .data(dataSet)
+  .enter()
+  .append("text");
+
+circleLabels
+  .attr("x", function (d, i) {
+    return (i + 1) * 25;
+  })
+
+  .attr("y", function (d, i) {
+    return (i + 1) * 25;
+  });
+
+circleLabels.text(function (d, i) {
+  return d.name;
+});
+
+circleLabels.style("fill", "green");
+
+// circleLabels.text(function (d, i) {
+//   return d.rank;
+// });
+circleLabels.text(function (d, i) {
+  return "Rank:" + d.rank + d.name;
+});
