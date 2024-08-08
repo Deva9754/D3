0ebfde3ad0a27cf05e1bd901c0347a12282313d7
Deva9754 <deva.sahu2001@gmail.com>
2024-08-08 19:02:02 +0530

    remove comment and axis added

--- linearScale/index.js
+++ linearScale/index.js
@@ -1,12 +1,9 @@
-// Set up the margins and dimensions
 var margin = { top: 50, right: 50, bottom: 50, left: 50 },
   width = 300 - margin.left - margin.right,
   height = 300 - margin.top - margin.bottom;
 
-// Create a linear scale
 var myScale = d3.scaleLinear().domain([0, 100]).range([0, width]);
 
-// Create the SVG container
 var mySVG = d3
   .select("body")
   .append("svg")
@@ -15,22 +12,18 @@ var mySVG = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Create and append the bottom axis
 var bottomAxis = d3.axisBottom(myScale);
 mySVG
   .append("g")
   .attr("transform", "translate(0," + height + ")")
   .call(bottomAxis);
 
-// Create and append the top axis
 var topAxis = d3.axisTop(myScale);
 mySVG.append("g").call(topAxis);
 
-// Create and append the left axis
 var leftAxis = d3.axisLeft(myScale);
 mySVG.append("g").call(leftAxis);
 
-// Create and append the right axis
 var rightAxis = d3.axisRight(myScale);
 mySVG
   .append("g")
