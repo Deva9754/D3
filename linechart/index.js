be379cf0ccf2a5201ed024f827f9599a3ed71aa7
Deva9754 <deva.sahu2001@gmail.com>
2024-08-27 18:11:43 +0530

    error fixed

--- linechart/index.js
+++ linechart/index.js
@@ -1,17 +1,16 @@
-var callbackError;
-var callbackData;
-
 var margin = { top: 20, right: 20, bottom: 30, left: 50 },
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
 var parseDate = d3.timeParse("%d-%b-%y");
+var formatYear = d3.timeFormat("%Y");
 
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
-var xAxis = d3.axisBottom().scale(x);
-var yAxis = d3.axisLeft().scale(y);
+var xAxis = d3.axisBottom(x).tickSize(0).tickFormat(formatYear).tickPadding(10);
+
+var yAxis = d3.axisLeft(y).tickSize(-width).tickPadding(10);
 
 var svg = d3
   .select("body")
@@ -21,41 +20,47 @@ var svg = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", function (error, data) {
-  if (error) throw error;
+var data = [
+  { date: "01-Jan-20", close: 150 },
+  { date: "02-Jan-21", close: 200 },
+];
 
-  callbackData = data;
-
-  callbackData.forEach(function (d) {
-    d.date = parseDate(d.date);
-    d.close = +d.close;
-  });
-
-  x.domain(
-    d3.extent(callbackData, function (d) {
-      return d.date;
-    })
-  );
-  y.domain(
-    d3.extent(callbackData, function (d) {
-      return d.close;
-    })
-  );
-
-  svg
-    .append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-  svg
-    .append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("price($)");
+data.forEach(function (d) {
+  d.date = parseDate(d.date);
+  d.close = +d.close;
 });
+
+x.domain(
+  d3.extent(data, function (d) {
+    return d.date;
+  })
+);
+y.domain(
+  d3.extent(data, function (d) {
+    return d.close;
+  })
+);
+
+svg
+  .append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis)
+  .append("text")
+  .attr("x", width / 2)
+  .attr("y", margin.bottom - 10)
+  .style("text-anchor", "middle")
+  .text("Year");
+
+svg
+  .append("g")
+  .attr("class", "y axis")
+  .call(yAxis)
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", -margin.left)
+  .attr("dy", ".71em")
+  .style("text-anchor", "middle")
+  .text("Price ($)");
+
+svg.append("path").datum(callbackData).attr("class", "line").attr("d", line);
